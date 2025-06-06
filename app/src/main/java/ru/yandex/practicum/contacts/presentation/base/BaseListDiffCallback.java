3b418771ccdf68923503f58b8e6916b41c1a25dc
irbis <irbis2496@ya.ru>
2025-06-06 22:17:27 +0300

    Revert "Project work stage 2,"
    This reverts commit 1093db296c924ac4ee46911c466381f423e2524d.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
-
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
     @Override
@@ -15,11 +13,5 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.equals(newItem);
     }
-    @Nullable
-    @Override
-    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        return newItem;
-    }
-
 
 }
